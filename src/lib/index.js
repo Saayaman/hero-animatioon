dd8abcdcabf7fb8f37a55292de84de7b3cc2062d
Ayako <ayakosayama1987@gmail.com>
2019-07-07 09:31:21 -0700

    update

---
+++ src/lib/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MyComponent.css'
+
+class MyComponent extends React.Component {
+  render(){
+    return (
+      <div className="MyComponent">This is MY COMPONENT!!!!!</div>
+    )
+  }
+}
+
+export default MyComponent
